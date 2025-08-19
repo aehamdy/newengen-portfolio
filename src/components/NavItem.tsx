f6342b30a51ea0e938fc728e52d40b318bd325c6
AhmedEssam <ae.devverse@gmail.com>
2025-08-19 18:36:29 +0300

    Chore: Update stylings

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 function NavItem({ navItem }: { navItem: NavItem }) {
   return (
-    <li className="group relative w-full md:w-fit py-[15px] px-[20px] md:p-0 font-bold md:font-normal text-content-light text-2xlarge md:text-compact border-b-[0.5px] first:border-t-[1px] md:border-none border-white/20">
-      <Link href={navItem.href} className="">
+    <li className="group relative flex md:w-fit hover:ps-3 md:hover:ps-0 font-bold md:font-normal text-content-light text-2xlarge md:text-compact border-b-[0.5px] first:border-t-[1px] md:border-none border-white/20 transition-all duration-slower">
+      <Link href={navItem.href} className="w-full py-[15px] px-[20px] md:p-0">
         {navItem.label.charAt(0).toUpperCase() + navItem.label.slice(1)}
       </Link>
 
@@ -12,7 +12,7 @@ function NavItem({ navItem }: { navItem: NavItem }) {
         className="hidden md:flex absolute left-0 -bottom-0.5 h-[1px] w-full bg-current 
             scale-x-0 transition-transform duration-300 ease-in-out
             group-hover:scale-x-100 
-            [transform-origin:0%] group-hover:[transform-origin:100%]"
+            [transform-origin:100%] group-hover:[transform-origin:0%]"
       />
     </li>
   );
