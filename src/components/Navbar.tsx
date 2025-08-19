d0f0f0842ab1d2729d53b119ab84bfd6606180aa
AhmedEssam <ae.devverse@gmail.com>
2025-08-19 17:56:30 +0300

    Feat: Create Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import NavItem from "@/components/NavItem";
+import { navList } from "@/data/navList";
+
+function Navbar() {
+  return (
+    <nav className="hidden md:flex bg-surface-primary px-sm py-xs rounded-sm">
+      <ul className="flex items-center gap-md">
+        {navList.map((item) => (
+          <NavItem key={item.id} navItem={item} />
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
