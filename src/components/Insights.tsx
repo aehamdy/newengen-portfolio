9e2a87d61cfb1aeb476d91c8d1d790e8c718b1d0
AhmedEssam <ae.devverse@gmail.com>
2025-08-27 19:53:09 +0300

    Feat: Create Insights section component

---
+++ src/components/Insights.tsx
@@ -0,0 +1,70 @@
+import SectionHeading from "@/components/SectionHeading";
+import { insights } from "@/data/insights";
+import { siteConfig } from "@/lib/config";
+import Link from "next/link";
+import Image from "next/image";
+
+function Insights() {
+  return (
+    <section
+      id="selected-work"
+      className="grid grid-cols-1 w-full min-h-dvh main-section-py main-section-px bg-surface-secondary"
+    >
+      <div className="flex flex-col items-center gap-xs md:gap-sm">
+        <SectionHeading color="light" align="center" className="">
+          <h3 className="font-black text-7xlarge md:text-8xlarge leading-14 md:leading-24 text-content-dark uppercase">
+            Insights By <br /> {siteConfig.name}
+          </h3>
+        </SectionHeading>
+
+        <p className="text-center text-content-dark">
+          Insights that drive impact—rooted in research, supported by data, and
+          made to fuel brand growth.
+        </p>
+
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-6">
+          {insights.map((insight) => (
+            <li key={insight.id} className="group text-content-dark">
+              <Link href={insight.link}>
+                <div className="rounded-md overflow-hidden">
+                  <Image
+                    src={insight.image}
+                    alt={insight.description}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+
+                <div className="flex flex-col items-start gap-4 mt-3">
+                  <div className="flex items-center gap-2 text-xlarge lg:text-base">
+                    <div className="flex items-center gap-1">
+                      {insight.tags.map((tag) => (
+                        <span
+                          key={tag}
+                          className="flex py-1 px-2 border border-dark rounded-4xl"
+                        >
+                          {tag}
+                        </span>
+                      ))}
+                    </div>
+
+                    <div className="">
+                      <p className="">{insight.date}</p>
+                    </div>
+                  </div>
+
+                  <div className="relative w-fit before:absolute before:w-0 group-hover:before:w-full before:h-full before:top-0 before:start-0 before:bg-accent-primary before:transition-all before:duration-500">
+                    <p className="relative group-hover:underline">
+                      {insight.description}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default Insights;
