069e086896c881efcc0a2ed5c7d4e7946355e7ab
AhmedEssam <ae.devverse@gmail.com>
2025-08-29 17:58:32 +0300

    Style: Adjust Logo styles

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import { siteConfig } from "@/lib/config";
 
 function Logo() {
   return (
-    <div className="p-xs text-content-light bg-surface-primary font-semibold text-2xl rounded-sm">
+    <div className="p-xs text-content-light bg-surface-primary font-semibold text-2xl rounded-md">
       {siteConfig.name.toUpperCase()}
     </div>
   );
