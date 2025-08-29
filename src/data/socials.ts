36139a58b5ff0c793084a57a14b7a95117bda08c
AhmedEssam <ae.devverse@gmail.com>
2025-08-29 17:45:49 +0300

    Feat: Create social data file

---
+++ src/data/socials.ts
@@ -0,0 +1,23 @@
+export type socialType = {
+  id: number;
+  title: string;
+  href: string;
+};
+
+export const socials = [
+  {
+    id: 1,
+    title: "LinkedIn",
+    href: "#",
+  },
+  {
+    id: 2,
+    title: "Facebook",
+    href: "#",
+  },
+  {
+    id: 3,
+    title: "Instagram",
+    href: "#",
+  },
+];
