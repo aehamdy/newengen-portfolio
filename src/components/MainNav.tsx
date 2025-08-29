74319bbf7f34b9bca23733e72565d86fb8af08ce
AhmedEssam <ae.devverse@gmail.com>
2025-08-29 17:59:10 +0300

    Adjust MainNav on mobile screen

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,14 +1,22 @@
 import Button from "@/components/Button";
+import Icon from "@/components/Icon";
 import Navbar from "@/components/Navbar";
 import NavMenu from "@/components/NavMenu";
 
 function MainNav() {
   return (
-    <div className="flex items-center gap-xs">
-      <div className="hidden md:flex">
+    <div className="flex items-center">
+      <div className="hidden md:flex md:gap-xs">
         <Navbar />
 
-        <Button isPrimary>Let&apos;s talk</Button>
+        <Button isPrimary>
+          <div className="">Let&apos;s talk</div>
+
+          <Icon
+            name="arrow-right"
+            className="group-hover:translate-x-1 transition-all"
+          />
+        </Button>
       </div>
 
       <NavMenu />
