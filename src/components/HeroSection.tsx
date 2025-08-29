7e7b3a90af692bb50c8f928aca1af46fa6043554
AhmedEssam <ae.devverse@gmail.com>
2025-08-29 17:55:58 +0300

    Feat: Add heading, text and button

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,13 +1,16 @@
+import Icon from "@/components/Icon";
+import Link from "next/link";
+
 function HeroSection() {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 h-dvh pt-[95px] pb-[80px] text-content-light">
+    <section className="relative grid grid-cols-1 md:grid-cols-2 h-dvh pt-[95px] main-section-px pb-[80px] text-content-light">
       <div className="flex flex-col md:justify-between gap-md">
-        <h1 className="flex flex-col gap-0 font-black text-[65px] md:text-8xlarge">
-          <div className="leading-[65px] md:leading-22">GENUINE.</div>
-          <div className="leading-[65px] md:leading-22">IMPACT.</div>
+        <h1 className="flex flex-col gap-0 font-black text-[55px] md:text-[120px]">
+          <div className="leading-[50px] md:leading-[110px]">GENUINE.</div>
+          <div className="leading-[50px] md:leading-[110px]">IMPACT.</div>
         </h1>
 
-        <p className="md:w-7/10 text-[20px] md:text-xlarge">
+        <p className="md:w-7/10 text-base md:text-xlarge">
           We are an industry-leading digital marketing agency partnering with
           bold brands to drive impact across every stage of the customer journey
           - maximizing it, measuring it, and repeating it.
@@ -15,6 +18,13 @@ function HeroSection() {
       </div>
 
       <div className=""></div>
+
+      <Link
+        href="#selected-work"
+        className="absolute start-1/2 bottom-3 translate-x-1/2 flex animate-bounce"
+      >
+        <Icon name="arrows-down" />
+      </Link>
     </section>
   );
 }
