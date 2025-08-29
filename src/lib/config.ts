725b506fc4987d109f739b4ddb25f8ec217272b2
AhmedEssam <ae.devverse@gmail.com>
2025-08-29 18:05:28 +0300

    Feat: Create config file that contains website info

---
+++ src/lib/config.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: "New Engen",
+  // description: "Creative digital agency helping brands grow.",
+  // url: "https://newengen.com",
+  // contactEmail: "hello@newengen.com",
+  // social: {
+  //   twitter: "https://twitter.com/newengen",
+  //   linkedin: "https://linkedin.com/company/newengen",
+  // },
+};
